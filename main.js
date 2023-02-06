56ab6138e1ddcfede06e91397f25df5c6d6fcfd0
Tzepes <andreicristian.vlad@gmail.com>
2023-02-06 12:20:48 +0200

    huge shader simplification

--- main.js
+++ main.js
@@ -2,27 +2,13 @@
 
 var vertexShaderGLSL = [
   'uniform mat4 u_worldViewProjection;',
-   'uniform vec3 u_lightWorldPos;', // delete
-   'uniform mat4 u_world;',
-   'uniform mat4 u_viewInverse;',
-   'uniform mat4 u_worldInverseTranspose;',
    '',
    'attribute vec4 a_position;',
-   'attribute vec3 a_normal;',
-   'attribute vec2 a_texcoord;',
    '',
    'varying vec4 v_position;',
-   'varying vec2 v_texCoord;',
-   'varying vec3 v_normal;',
-   'varying vec3 v_surfaceToLight;',
-   'varying vec3 v_surfaceToView;',
    '',
    'void main() {',
-     'v_texCoord = a_texcoord;',
      'v_position = (u_worldViewProjection * a_position);',
-     'v_normal = (u_worldInverseTranspose * vec4(a_normal, 0)).xyz;',
-     'v_surfaceToLight = u_lightWorldPos - (u_world * a_position).xyz;',
-     'v_surfaceToView = (u_viewInverse[3] - (u_world * a_position)).xyz;',
      'gl_Position = v_position;',
    '}'
 ].join('\n');
@@ -30,45 +16,12 @@ var vertexShaderGLSL = [
 var fragmentShaderGLSL = [
   'precision mediump float;',
   '',
-  'varying vec4 v_position;',
-  'varying vec2 v_texCoord;',
-  'varying vec3 v_normal;',
-  'varying vec3 v_surfaceToLight;',
-  'varying vec3 v_surfaceToView;',
-  '',
-  'uniform vec4 u_lightColor;', // delete
-  'uniform vec4 u_colorMult;',
-  'uniform sampler2D u_diffuse;',
-  'uniform vec4 u_specular;',
-  'uniform float u_shininess;',
-  'uniform float u_specularFactor;',
-  '',
-  'vec4 lit(float l ,float h, float m) {',
-    'return vec4(1.0,',
-      'abs(l),',
-      '(l > 0.0) ? pow(max(0.0, h), m) : 0.0,',
-      '1.0);',
-  '}',
-  '',
   'void main() {',
-  '  vec4 diffuseColor = texture2D(u_diffuse, v_texCoord);',
-  '  vec3 a_normal = normalize(v_normal);',
-  '  vec3 surfaceToLight = normalize(v_surfaceToLight);',
-  '  vec3 surfaceToView = normalize(v_surfaceToView);',
-  '  vec3 halfVector = normalize(surfaceToLight + surfaceToView);',
-  '  vec4 litR = lit(dot(a_normal, surfaceToLight),',
-  '                    dot(a_normal, halfVector), u_shininess);',
-  '  vec4 outColor = vec4((',
-  '  u_lightColor * (diffuseColor * litR.y * u_colorMult +',
-  '                u_specular * litR.z * u_specularFactor)).rgb,',
-  '      diffuseColor.a);',
-  '  gl_FragColor = outColor;',
+  'gl_FragColor = vec4(1,1,0,1);',
   '}'
 ].join('\n')
 
 function main() {
-  // Get A WebGL context
-  /** @type {HTMLCanvasElement} */
   var canvas = document.querySelector("#canvas");
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
@@ -139,13 +92,6 @@ function main() {
      normal:   { numComponents: 3, data: [0, 0, 1, 0, 0, 1, 0, 0, 1],        },
   };
   var bufferInfo = webglUtils.createBufferInfoFromArrays(gl, arrays);
-  // 0, 3, 0, //triangle 1
-  // 2, 0, 0, 
-  // -2, 0, 0, 
-  // 0, -1, 0, // triangle 2
-  // -2, 2, 0, 
-  // 2, 2, 0
-  // setup GLSL program
   var program = createProgram(gl, vertexShader, fragmentShader);
   var uniformSetters = webglUtils.createUniformSetters(gl, program);
   var attribSetters  = webglUtils.createAttributeSetters(gl, program);
@@ -187,7 +133,6 @@ function main() {
         u_shininess:             rand(500),        
       },
     });
-    console.log(objects[ii].xRotation);
   }
 
   function checkStarSector(range){
@@ -244,7 +189,7 @@ function main() {
 
     // Draw objects
     objects.forEach(function(object) {
-      object.xRotation = object.xRotation + checkStarSector(object.radius) * 10;
+      object.xRotation = object.xRotation + checkStarSector(object.radius) * 3;
       //console.log(object.xRotation);
       // Compute a position for this object based on the time.
       var worldMatrix = m4.xRotation(object.xRotation * time);
